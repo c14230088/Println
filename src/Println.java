459982a4ec4d3ebf4be9d3c61a647b907d9a9fc2
c14230088 <Tanaka272cool@gmail.com>
2023-08-10 19:40:59 +0700

    Initial commit

---
+++ src/Println.java
@@ -0,0 +1,14 @@
+public class Println {
+    public static void main(String[] args) {
+        System.out.println("    Hello World");
+
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama : Kevin Tanaka");
+        System.out.println("NRP : c14230088 ");
+        System.out.println("Mata Kuliah : Algoritma dan Pemerograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun Ajaran 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("-----------------");
+            }
+}
